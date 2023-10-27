48bb8a06d4534f59f54f7793a0a66e2f72d967c1
reddybhargava85 <149133924+reddybhargava85@users.noreply.github.com>
2023-10-27 14:13:06 -0500

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.5.1"  # Specify the desired version
+    }
+
   }
 }
 
