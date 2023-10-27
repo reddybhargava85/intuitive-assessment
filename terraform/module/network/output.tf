54282368b0f6d4ba6ab562df6c3e926137554fcd
reddybhargava85 <149133924+reddybhargava85@users.noreply.github.com>
2023-10-27 16:12:31 -0500

    Delete terraform directory

--- terraform/module/network/output.tf
+++
@@ -1,7 +0,0 @@
-output "vnet-name" {
-  value = azurerm_virtual_network.vnet.name
-}
-
-output "nics" {
-  value = toset(values(azurerm_network_interface.netowrk-interface)[*].id)
-}
--- terraform/module/storage/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "sa-name" { }
-variable "resource_group_name" { }
-variable "location" { }
-variable "account_tier" { }
-variable "account_replication_type" {} #GRS
-variable "common-tags" {}
